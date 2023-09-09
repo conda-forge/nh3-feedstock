a601d7d6a0d2289c2a0a1860da8d9d3ad0d7058e
Nicholas Bollweg <nick.bollweg@gmail.com>
2023-09-09 13:04:52 -0500

    try pip

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,8 +2,10 @@
 
 set -ex
 
-maturin build --release --strip --manylinux off --interpreter="${PYTHON}" --out dist
+# maturin build --release --strip --manylinux off --interpreter="${PYTHON}" --out dist
 
-"${PYTHON}" -m pip install $SRC_DIR/dist/nh3*.whl --no-deps -vv
+# "${PYTHON}" -m pip install $SRC_DIR/dist/nh3*.whl --no-deps -vv
+
+$PYTHON -m pip install . -vv --no-deps --no-build-isolation
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
