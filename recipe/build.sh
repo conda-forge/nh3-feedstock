4e4e6792d540486ca9feb25bf1014dd4d9ea15aa
Nicholas Bollweg <nick.bollweg@gmail.com>
2023-09-09 13:33:40 -0500

    only use pip install for osx-arm64

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,10 +2,13 @@
 
 set -ex
 
-# maturin build --release --strip --manylinux off --interpreter="${PYTHON}" --out dist
-
-# "${PYTHON}" -m pip install $SRC_DIR/dist/nh3*.whl --no-deps -vv
-
-$PYTHON -m pip install . -vv --no-deps --no-build-isolation
+if [[ "${target_platform}" == "osx-arm64" ]]; then
+  # avoid "Bad CPU"
+  $PYTHON -m pip install . -vv --no-deps --no-build-isolation
+else
+  # prefer interpreter-specific builds
+  maturin build --release --strip --manylinux off --interpreter "${PYTHON}" --out dist
+  "${PYTHON}" -m pip install "${SRC_DIR}/dist/nh3*.whl" --no-deps -vv
+fi
 
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
