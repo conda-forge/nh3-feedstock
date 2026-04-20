73af21e44e9179fa742951e24175baeeb8b5d4a8
Nicholas Bollweg <nick.bollweg@gmail.com>
2026-04-20 09:40:12 -0500

    use native runners, pixi, remove build scripts

--- recipe/build-nh3.bat
+++
@@ -1,9 +0,0 @@
-@echo on
-set CARGO_PROFILE_RELEASE_STRIP=symbols
-set CARGO_PROFILE_RELEASE_LTO=fat
-
-%PYTHON% -m pip install . -vv --no-deps --no-build-isolation --disable-pip-version-check ^
-    || exit 1
-
-cargo-bundle-licenses --format yaml --output THIRDPARTY.yml ^
-    || exit 1
